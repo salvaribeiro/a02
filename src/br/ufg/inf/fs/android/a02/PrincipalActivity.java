40ea40204775efd08c640d8bda9d5a671444c24e
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-22 10:26:00 -0300

    Menu da atividade principal exibido corretamente.

--- src/br/ufg/inf/fs/android/a02/PrincipalActivity.java
+++ src/br/ufg/inf/fs/android/a02/PrincipalActivity.java
@@ -2,6 +2,8 @@ package br.ufg.inf.fs.android.a02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PrincipalActivity extends Activity {
     /**
@@ -11,5 +13,16 @@ public class PrincipalActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
     }
 }
