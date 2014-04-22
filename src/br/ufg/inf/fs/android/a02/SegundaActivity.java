d7f72124afd4ffa59bc865bb440d14877325daf2
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-22 10:42:40 -0300

    Opções de menu da segunda atividade implementados.

--- src/br/ufg/inf/fs/android/a02/SegundaActivity.java
+++ src/br/ufg/inf/fs/android/a02/SegundaActivity.java
@@ -1,8 +1,10 @@
 package br.ufg.inf.fs.android.a02;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by fabio_000 on 22/04/2014.
@@ -17,5 +19,22 @@ public class SegundaActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_segunda, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menusegunda_terceira) {
+            Intent intent = new Intent(this, TerceiraActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.menusegunda_fechar) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
