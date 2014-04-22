1427cb54a648e4a55bf737eb9f863da14ee8af87
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-22 10:18:07 -0300

    Terceira atividade criada.

---
+++ src/br/ufg/inf/fs/android/a02/TerceiraActivity.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.fs.android.a02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fabio_000 on 22/04/2014.
+ */
+public class TerceiraActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
